899d9602ae101cecf12dac61336ef2b8a4fbdd88
Omkar1683 <omramjadhav2003@gmail.com>
2026-04-01 23:34:19 +0530

    completed login page

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,14 @@
+import { QueryClient, useMutation, useQueryClient } from '@tanstack/react-query';
+import React from 'react'
+import { login } from '../lib/api';
+
+const useLogin = () => {
+    const QueryClient = useQueryClient();
+  const { mutate, isPending, error } = useMutation({
+     mutationFn: login,
+     onSuccess: () => QueryClient.invalidateQueries({ queryKey: ["authUser"] })
+   });
+   return {error,isPending,loginMutation:mutate};
+}
+
+export default useLogin
